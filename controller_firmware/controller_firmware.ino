7ec234719efb66e13d84efecb2eb11b53949fb0e
MetalStriker <metal.striker.xlr@gmail.com>
2023-02-04 00:10:12 +0500

    Implemented UART communication and Relay Control

---
+++ controller_firmware/controller_firmware.ino
@@ -0,0 +1,246 @@
+#include "global.h"
+
+String cmd = "";
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  Serial1.setTimeout(200);
+
+  // Send Reset command to UI
+  Serial1.print("xFFh");
+
+  defineRelayPins();
+
+  // Initialize all Relay pins as outputs and set their initial state to low (false)
+  for (int i = 0; i < numRelays; i++) {
+    pinMode(Relay[i], OUTPUT);
+    RelayState[i] = false;
+
+    digitalWrite(Relay[i], RelayState[i]);
+  }
+}
+
+void loop() {
+
+  if (Serial1.available() > 0) {
+
+    cmd = Serial1.readString();
+
+    if ((cmd[0] == 'x') && (cmd[3] == 'h'))
+    {
+      Serial.print("CMD Recv: ");
+      Serial.println(cmd);
+
+      if (cmd == "x00h") {
+        RelayState[0] = !RelayState[0];
+        digitalWrite(Relay[0], RelayState[0]);
+        Serial.println("Relay 0 Triggered");
+      }
+
+      if (cmd == "x01h") {
+        RelayState[1] = !RelayState[1];
+        digitalWrite(Relay[1], RelayState[1]);
+        Serial.println("Relay 1 Triggered");
+      }
+
+      if (cmd == "x02h") {
+        RelayState[2] = !RelayState[2];
+        digitalWrite(Relay[2], RelayState[2]);
+        Serial.println("Relay 2 Triggered");
+      }
+
+      if (cmd == "x03h") {
+        RelayState[3] = !RelayState[3];
+        digitalWrite(Relay[3], RelayState[3]);
+        Serial.println("Relay 3 Triggered");
+      }
+
+      if (cmd == "x04h") {
+        RelayState[4] = !RelayState[4];
+        digitalWrite(Relay[4], RelayState[4]);
+        Serial.println("Relay 4 Triggered");
+      }
+
+      if (cmd == "x05h") {
+        RelayState[5] = !RelayState[5];
+        digitalWrite(Relay[5], RelayState[5]);
+        Serial.println("Relay 5 Triggered");
+      }
+
+      if (cmd == "x06h") {
+        RelayState[6] = !RelayState[6];
+        digitalWrite(Relay[6], RelayState[6]);
+        Serial.println("Relay 6 Triggered");
+      }
+
+      if (cmd == "x07h") {
+        RelayState[7] = !RelayState[7];
+        digitalWrite(Relay[7], RelayState[7]);
+        Serial.println("Relay 7 Triggered");
+      }
+
+      if (cmd == "x08h") {
+        RelayState[8] = !RelayState[8];
+        digitalWrite(Relay[8], RelayState[8]);
+        Serial.println("Relay 8 Triggered");
+      }
+
+      if (cmd == "x09h") {
+        RelayState[9] = !RelayState[9];
+        digitalWrite(Relay[9], RelayState[9]);
+        Serial.println("Relay 9 Triggered");
+      }
+
+      if (cmd == "x0Ah") {
+        RelayState[10] = !RelayState[10];
+        digitalWrite(Relay[10], RelayState[10]);
+        Serial.println("Relay 10 Triggered");
+      }
+
+      if (cmd == "x0Bh") {
+        RelayState[11] = !RelayState[11];
+        digitalWrite(Relay[11], RelayState[11]);
+        Serial.println("Relay 11 Triggered");
+      }
+
+      if (cmd == "x0Ch") {
+        RelayState[12] = !RelayState[12];
+        digitalWrite(Relay[12], RelayState[12]);
+        Serial.println("Relay 12 Triggered");
+      }
+
+      if (cmd == "x0Dh") {
+        RelayState[13] = !RelayState[13];
+        digitalWrite(Relay[13], RelayState[13]);
+        Serial.println("Relay 13 Triggered");
+      }
+
+      if (cmd == "x0Eh") {
+        RelayState[14] = !RelayState[14];
+        digitalWrite(Relay[14], RelayState[14]);
+        Serial.println("Relay 14 Triggered");
+      }
+
+      if (cmd == "x0Fh") {
+        RelayState[15] = !RelayState[15];
+        digitalWrite(Relay[15], RelayState[15]);
+        Serial.println("Relay 15 Triggered");
+      }
+
+      if (cmd == "x10h") {
+        RelayState[16] = !RelayState[16];
+        digitalWrite(Relay[16], RelayState[16]);
+        Serial.println("Relay 16 Triggered");
+      }
+
+      if (cmd == "x11h") {
+        RelayState[17] = !RelayState[17];
+        digitalWrite(Relay[17], RelayState[17]);
+        Serial.println("Relay 17 Triggered");
+      }
+
+      if (cmd == "x12h") {
+        RelayState[18] = !RelayState[18];
+        digitalWrite(Relay[18], RelayState[18]);
+        Serial.println("Relay 18 Triggered");
+      }
+
+      if (cmd == "x13h") {
+        RelayState[19] = !RelayState[19];
+        digitalWrite(Relay[19], RelayState[19]);
+        Serial.println("Relay 19 Triggered");
+      }
+
+      if (cmd == "x14h") {
+        RelayState[20] = !RelayState[20];
+        digitalWrite(Relay[20], RelayState[20]);
+        Serial.println("Relay 20 Triggered");
+      }
+
+      if (cmd == "x15h") {
+        RelayState[21] = !RelayState[21];
+        digitalWrite(Relay[21], RelayState[21]);
+        Serial.println("Relay 21 Triggered");
+      }
+
+      if (cmd == "x16h") {
+        RelayState[22] = !RelayState[22];
+        digitalWrite(Relay[22], RelayState[22]);
+        Serial.println("Relay 22 Triggered");
+      }
+
+      if (cmd == "x17h") {
+        RelayState[23] = !RelayState[23];
+        digitalWrite(Relay[23], RelayState[23]);
+        Serial.println("Relay 23 Triggered");
+      }
+
+      if (cmd == "x18h") {
+        RelayState[24] = !RelayState[24];
+        digitalWrite(Relay[24], RelayState[24]);
+        Serial.println("Relay 24 Triggered");
+      }
+
+      if (cmd == "x19h") {
+        RelayState[25] = !RelayState[25];
+        digitalWrite(Relay[25], RelayState[25]);
+        Serial.println("Relay 25 Triggered");
+      }
+
+      if (cmd == "x1Ah") {
+        RelayState[26] = !RelayState[26];
+        digitalWrite(Relay[26], RelayState[26]);
+        Serial.println("Relay 26 Triggered");
+      }
+
+      if (cmd == "x1Bh") {
+        RelayState[27] = !RelayState[27];
+        digitalWrite(Relay[27], RelayState[27]);
+        Serial.println("Relay 27 Triggered");
+      }
+
+      if (cmd == "x1Ch") {
+        RelayState[28] = !RelayState[28];
+        digitalWrite(Relay[28], RelayState[28]);
+        Serial.println("Relay 28 Triggered");
+      }
+
+      if (cmd == "x1Dh") {
+        RelayState[29] = !RelayState[29];
+        digitalWrite(Relay[29], RelayState[29]);
+        Serial.println("Relay 29 Triggered");
+      }
+
+      if (cmd == "x1Eh") {
+        RelayState[30] = !RelayState[30];
+        digitalWrite(Relay[30], RelayState[30]);
+        Serial.println("Relay 30 Triggered");
+      }
+
+      if (cmd == "x1Fh") {
+        RelayState[31] = !RelayState[31];
+        digitalWrite(Relay[31], RelayState[31]);
+        Serial.println("Relay 31 Triggered");
+      }
+
+      if (cmd == "x20h") {
+        RelayState[32] = !RelayState[32];
+        digitalWrite(Relay[32], RelayState[32]);
+        Serial.println("Relay 32 Triggered");
+      }
+
+      if (cmd == "x21h") {
+        RelayState[33] = !RelayState[33];
+        digitalWrite(Relay[33], RelayState[33]);
+        Serial.println("Relay 33 Triggered");
+      }
+
+      if (cmd == "x22h") {
+        RelayState[34] = !RelayState[34];
+        digitalWrite(Relay[34], RelayState[34]);
+        Serial.println("Relay 34 Triggered");
+      }
+    }
+  }
+}
---
+++ controller_firmware/global.h
@@ -0,0 +1,47 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+const int numRelays = 35;
+int Relay[numRelays];
+bool RelayState[numRelays];
+
+// Define Relay Pin Connections
+void defineRelayPins() {
+  Relay[0] = 2;
+  Relay[1] = 3;
+  Relay[2] = 4;
+  Relay[3] = 22;
+  Relay[4] = 23;
+  Relay[5] = 24;
+  Relay[6] = 25;
+  Relay[7] = 26;
+  Relay[8] = 27;
+  Relay[9] = 28;
+  Relay[10] = 29;
+  Relay[11] = 30;
+  Relay[12] = 31;
+  Relay[13] = 32;
+  Relay[14] = 33;
+  Relay[15] = 34;
+  Relay[16] = 35;
+  Relay[17] = 36;
+  Relay[18] = 37;
+  Relay[19] = 38;
+  Relay[20] = 39;
+  Relay[21] = 40;
+  Relay[22] = 41;
+  Relay[23] = 42;
+  Relay[24] = 43;
+  Relay[25] = 44;
+  Relay[26] = 45;
+  Relay[27] = 46;
+  Relay[28] = 47;
+  Relay[29] = 48;
+  Relay[30] = 49;
+  Relay[31] = 50;
+  Relay[32] = 51;
+  Relay[33] = 52;
+  Relay[34] = 53;
+}
+
+#endif
